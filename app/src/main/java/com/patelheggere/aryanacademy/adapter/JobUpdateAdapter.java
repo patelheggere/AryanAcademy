ed9737d9fd4f998ad3b5126bcf4323c7b0b7a0e2
India Wyn Android <patelheggere@gmail.com>
2018-09-21 12:21:52 +0530

    job updates

--- app/src/main/java/com/patelheggere/aryanacademy/adapter/JobUpdateAdapter.java
+++ app/src/main/java/com/patelheggere/aryanacademy/adapter/JobUpdateAdapter.java
@@ -9,55 +9,91 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.patelheggere.aryanacademy.R;
+import com.patelheggere.aryanacademy.model.JobUpdatesModel;
 
 import java.util.List;
 
-public class CurrentAffairsAdapter extends RecyclerView.Adapter<CurrentAffairsAdapter.CurrentaffairsViewHolder>{
+public class JobUpdateAdapter extends RecyclerView.Adapter<JobUpdateAdapter.CurrentaffairsViewHolder>{
 
-    List<String> dateList;
-    private List<String> messageList;
+    List<JobUpdatesModel> jobList;
     private Context mContext;
 
-    public CurrentAffairsAdapter(Context mContext, List<String> dateList, List<String> messageList) {
-        this.dateList = dateList;
-        this.messageList = messageList;
+    public JobUpdateAdapter(Context mContext, List<JobUpdatesModel> jobList) {
+        this.jobList = jobList;
         this.mContext = mContext;
     }
 
     @NonNull
     @Override
     public CurrentaffairsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.current_affairs_item, viewGroup, false);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.job_item, viewGroup, false);
         return new CurrentaffairsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CurrentaffairsViewHolder currentaffairsViewHolder, int i) {
-        String date = dateList.get(i);
-        if(date!=null)
-        {
-            currentaffairsViewHolder.textViewDate.setText(date);
-        }
-        String details = messageList.get(i);
-        if(details!=null)
-        {
-            currentaffairsViewHolder.textViewDetails.setText(details);
-        }
+       JobUpdatesModel jobUpdatesModel = jobList.get(i);
+       if(jobUpdatesModel.getDept()!=null)
+           currentaffairsViewHolder.dept.setText(jobUpdatesModel.getDept());
+
+        if(jobUpdatesModel.getDocuments()!=null)
+            currentaffairsViewHolder.documents.setText(jobUpdatesModel.getDocuments());
+
+        if(jobUpdatesModel.getWebsite()!=null)
+            currentaffairsViewHolder.website.setText(jobUpdatesModel.getWebsite());
+
+        if(jobUpdatesModel.getExamDate()!=null)
+            currentaffairsViewHolder.examDate.setText(jobUpdatesModel.getExamDate());
+
+        if(jobUpdatesModel.getExamMode()!=null)
+            currentaffairsViewHolder.examMode.setText(jobUpdatesModel.getExamMode());
+
+
+        if(jobUpdatesModel.getLastDate()!=null)
+            currentaffairsViewHolder.lastDate.setText(jobUpdatesModel.getLastDate());
+
+        if(jobUpdatesModel.getNoPosts()!=null)
+            currentaffairsViewHolder.noPost.setText(jobUpdatesModel.getNoPosts());
+
+        if(jobUpdatesModel.getNotiDate()!=null)
+            currentaffairsViewHolder.noti_date.setText(jobUpdatesModel.getNotiDate());
+
+        if(jobUpdatesModel.getPaymentMode()!=null)
+            currentaffairsViewHolder.payment.setText(jobUpdatesModel.getPaymentMode());
+
+        if(jobUpdatesModel.getQualification()!=null)
+            currentaffairsViewHolder.qualification.setText(jobUpdatesModel.getQualification());
+
+        if(jobUpdatesModel.getTitle()!=null)
+            currentaffairsViewHolder.title.setText(jobUpdatesModel.getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return dateList.size();
+        return jobList.size();
     }
 
     public class CurrentaffairsViewHolder extends RecyclerView.ViewHolder {
 
-        TextView textViewDate;
-        TextView textViewDetails;
+        TextView title, dept, qualification, noti_date, lastDate, examMode, payment, documents, examDate, noPost, website;
         public CurrentaffairsViewHolder(@NonNull View itemView) {
             super(itemView);
-            textViewDate = itemView.findViewById(R.id.tv_date);
-            textViewDetails = itemView.findViewById(R.id.tv_details);
+            title = itemView.findViewById(R.id.tv_job_title);
+            dept = itemView.findViewById(R.id.tv_dept);
+
+            qualification = itemView.findViewById(R.id.tv_qualifiacation_value);
+            noti_date = itemView.findViewById(R.id.tv_noti_date_value);
+
+            lastDate = itemView.findViewById(R.id.tv_last_date_value);
+            examMode  = itemView.findViewById(R.id.tv_mode_of_exam_value);
+
+            payment = itemView.findViewById(R.id.tv_payment_mode_value);
+            documents = itemView.findViewById(R.id.tv_documents_value);
+
+            examDate = itemView.findViewById(R.id.tv_exam_date_value);
+            noPost = itemView.findViewById(R.id.tv_no_of_post_value);
+
+            website = itemView.findViewById(R.id.tv_web_value);
         }
     }
 }
--- app/src/main/java/com/patelheggere/aryanacademy/fragments/jobs/JobUpdatesFragment.java
+++ app/src/main/java/com/patelheggere/aryanacademy/fragments/jobs/JobUpdatesFragment.java
@@ -3,47 +3,54 @@ package com.patelheggere.aryanacademy.fragments.jobs;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.patelheggere.aryanacademy.R;
+import com.patelheggere.aryanacademy.adapter.CurrentAffairsAdapter;
+import com.patelheggere.aryanacademy.adapter.JobUpdateAdapter;
 import com.patelheggere.aryanacademy.base.BaseFragment;
+import com.patelheggere.aryanacademy.helper.SharedPrefsHelper;
+import com.patelheggere.aryanacademy.model.CurrentAffairsModel;
+import com.patelheggere.aryanacademy.model.JobUpdatesModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link JobUpdatesFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link JobUpdatesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.patelheggere.aryanacademy.helper.AppUtils.Constants.JOB;
+import static com.patelheggere.aryanacademy.helper.AppUtils.Constants.LANGUAGE;
+
 public class JobUpdatesFragment extends BaseFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private View mView;
+    private RecyclerView recyclerViewJobs;
+    private ProgressBar mProgressBar;
+    private List<JobUpdatesModel> jobUpdatesModelList;
+    private DatabaseReference databaseReference;
+
     private OnFragmentInteractionListener mListener;
+    private JobUpdateAdapter adapter;
 
     public JobUpdatesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment JobUpdatesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static JobUpdatesFragment newInstance(String param1, String param2) {
         JobUpdatesFragment fragment = new JobUpdatesFragment();
         Bundle args = new Bundle();
@@ -66,10 +73,58 @@ public class JobUpdatesFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job_updates, container, false);
+        mView =  inflater.inflate(R.layout.fragment_job_updates, container, false);
+        initViews();
+        getJobDetails();
+        return mView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    private void initViews()
+    {
+        jobUpdatesModelList = new ArrayList<>();
+        mProgressBar = mView.findViewById(R.id.progress_bar);
+        recyclerViewJobs = mView.findViewById(R.id.rv_jobs);
+    }
+
+    private void getJobDetails() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        String lang = SharedPrefsHelper.getInstance().get(LANGUAGE, "ka");
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        /*JobUpdatesModel ob = new JobUpdatesModel();
+        ob.setTitle("PDO");
+        ob.setDept("KPSC");
+        ob.setDocuments("All marks card");
+        ob.setExamDate("3010-2018");
+        ob.setExamMode("Offline");
+        ob.setLastDate("30-09-2018");
+        ob.setNoPosts("400");
+        ob.setNotiDate("30-08-2018");
+        ob.setPaymentMode("post office challan");
+        ob.setQualification("Any degree");
+        ob.setWebsite("http://www.kpsc.kar.nic.in");
+        databaseReference.child(lang).child(JOB).push().setValue(ob);*/
+        databaseReference = databaseReference.child(lang).child(JOB);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren())
+                {
+                    jobUpdatesModelList.add(snapshot.getValue(JobUpdatesModel.class));
+                }
+                adapter = new JobUpdateAdapter(mActivity, jobUpdatesModelList);
+                recyclerViewJobs.setLayoutManager(new LinearLayoutManager(mActivity, LinearLayoutManager.VERTICAL, false));
+                recyclerViewJobs.setAdapter(adapter);
+                mProgressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -93,16 +148,6 @@ public class JobUpdatesFragment extends BaseFragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
